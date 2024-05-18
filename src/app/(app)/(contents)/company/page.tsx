36c0fdc90bef2089093c0cfe8096888c6fc0ce96
genki1117 <88384341+genki1117@users.noreply.github.com>
2024-05-18 18:42:08 +0900

    property fix

--- src/app/(app)/(contents)/company/page.tsx
+++ src/app/(app)/(contents)/company/page.tsx
@@ -38,7 +38,7 @@ const Company = () => {
                     </div>
                 </div>
                 <div className='company-map w-full flex justify-center mt-5'>
-                    <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3281.2537991596773!2d135.14557790931045!3d34.673543442878746!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x60008589021e867f%3A0x8f0d6621640900d0!2z44CSNjUzLTA4MTIg5YW15bqr55yM56We5oi45biC6ZW355Sw5Yy66ZW355Sw55S677yV5LiB55uu77yT4oiS77yS!5e0!3m2!1sja!2sjp!4v1716024902308!5m2!1sja!2sjp" width="800" height="600" allowfullscreen="" loading="lazy" referrerpolicy="no-referrer-when-downgrade"></iframe>
+                    <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3281.2537991596773!2d135.14557790931045!3d34.673543442878746!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x60008589021e867f%3A0x8f0d6621640900d0!2z44CSNjUzLTA4MTIg5YW15bqr55yM56We5oi45biC6ZW355Sw5Yy66ZW355Sw55S677yV5LiB55uu77yT4oiS77yS!5e0!3m2!1sja!2sjp!4v1716024902308!5m2!1sja!2sjp" width="800" height="600" allowFullScreen={true} loading="lazy" referrerPolicy="no-referrer-when-downgrade"></iframe>
                 </div>
             </div>
         </Contents>
