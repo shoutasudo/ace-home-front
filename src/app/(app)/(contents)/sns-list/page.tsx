5fb2a091829f07cde0ea16c67e476ed8dc9abeba
genki1117 <88384341+genki1117@users.noreply.github.com>
2024-05-19 22:01:15 +0900

    sns-list fix

--- src/app/(app)/(contents)/sns-list/page.tsx
+++ src/app/(app)/(contents)/sns-list/page.tsx
@@ -10,7 +10,7 @@ const SnsListPage = () => {
                     <div className="w-3/12 sm:w-full">
                         <div className="sns-list-card-content-sns-card-icon flex justify-center items-center">
                             <Image
-                                src="/images/66x66.png"
+                                src="/socal-logo/LINE.png"
                                 width={66}
                                 height={66}
                                 alt="icon"
@@ -22,7 +22,7 @@ const SnsListPage = () => {
                     </div>
                     <div className="sns-list-card-content-sns-card-description w-9/12 pl-2 sm:w-full sm:pl-0 sm:flex sm:justify-center sm:mt-2">
                         <p className="text-xs tracking-wide noto-sans-jp">
-                            ひとり暮らし女性に嬉しいキャンペーン情報やおトクなクーポンをお届けします！（LINEのお友達登録要）
+                            おすすめ物件やお得なキャンペーン情報を先行配信いたします！（LINEのお友達登録要）
                         </p>
                     </div>
                 </div>
@@ -30,19 +30,19 @@ const SnsListPage = () => {
                     <div className="w-3/12 sm:w-full">
                         <div className="sns-list-card-content-sns-card-icon flex justify-center items-center">
                             <Image
-                                src="/images/66x66.png"
+                                src="/socal-logo/TikTok.png"
                                 width={66}
                                 height={66}
                                 alt="icon"
                             />
                         </div>
                         <div className="sns-list-card-content-sns-card-title hidden sm:flex sm: justify-center sm:mt-3">
-                            <h3>LINE</h3>
+                            <h3>TikTok</h3>
                         </div>
                     </div>
                     <div className="sns-list-card-content-sns-card-description w-9/12 pl-2 sm:w-full sm:pl-0 sm:flex sm:justify-center sm:mt-2">
                         <p className="text-xs tracking-wide noto-sans-jp">
-                            ひとり暮らし女性に嬉しいキャンペーン情報やおトクなクーポンをお届けします！（LINEのお友達登録要）
+                            物件の内装やお部屋を動画で公開！お写真で見るより断然わかりやすはず！
                         </p>
                     </div>
                 </div>
@@ -50,19 +50,19 @@ const SnsListPage = () => {
                     <div className="w-3/12 sm:w-full">
                         <div className="sns-list-card-content-sns-card-icon flex justify-center items-center">
                             <Image
-                                src="/images/66x66.png"
+                                src="/socal-logo/Instagram.png"
                                 width={66}
                                 height={66}
                                 alt="icon"
                             />
                         </div>
                         <div className="sns-list-card-content-sns-card-title hidden sm:flex sm: justify-center sm:mt-3">
-                            <h3>LINE</h3>
+                            <h3>Instagram</h3>
                         </div>
                     </div>
                     <div className="sns-list-card-content-sns-card-description w-9/12 pl-2 sm:w-full sm:pl-0 sm:flex sm:justify-center sm:mt-2">
                         <p className="text-xs tracking-wide noto-sans-jp">
-                            ひとり暮らし女性に嬉しいキャンペーン情報やおトクなクーポンをお届けします！（LINEのお友達登録要）
+                            ACE-HOMEのいろんな特典を先行配信！Instagramだけの特典も・・・
                         </p>
                     </div>
                 </div>
@@ -70,59 +70,19 @@ const SnsListPage = () => {
                     <div className="w-3/12 sm:w-full">
                         <div className="sns-list-card-content-sns-card-icon flex justify-center items-center">
                             <Image
-                                src="/images/66x66.png"
+                                src="/socal-logo/x.png"
                                 width={66}
                                 height={66}
                                 alt="icon"
                             />
                         </div>
                         <div className="sns-list-card-content-sns-card-title hidden sm:flex sm: justify-center sm:mt-3">
-                            <h3>LINE</h3>
+                            <h3>X</h3>
                         </div>
                     </div>
                     <div className="sns-list-card-content-sns-card-description w-9/12 pl-2 sm:w-full sm:pl-0 sm:flex sm:justify-center sm:mt-2">
                         <p className="text-xs tracking-wide noto-sans-jp">
-                            ひとり暮らし女性に嬉しいキャンペーン情報やおトクなクーポンをお届けします！（LINEのお友達登録要）
-                        </p>
-                    </div>
-                </div>
-                <div className="sns-list-card-content-sns-card w-full py-6 px-5 mt-3 bg-white ring-mainPink rounded-lg flex justify-center sm:flex-col items-center ring-4">
-                    <div className="w-3/12 sm:w-full">
-                        <div className="sns-list-card-content-sns-card-icon flex justify-center items-center">
-                            <Image
-                                src="/images/66x66.png"
-                                width={66}
-                                height={66}
-                                alt="icon"
-                            />
-                        </div>
-                        <div className="sns-list-card-content-sns-card-title hidden sm:flex sm: justify-center sm:mt-3">
-                            <h3>LINE</h3>
-                        </div>
-                    </div>
-                    <div className="sns-list-card-content-sns-card-description w-9/12 pl-2 sm:w-full sm:pl-0 sm:flex sm:justify-center sm:mt-2">
-                        <p className="text-xs tracking-wide noto-sans-jp">
-                            ひとり暮らし女性に嬉しいキャンペーン情報やおトクなクーポンをお届けします！（LINEのお友達登録要）
-                        </p>
-                    </div>
-                </div>
-                <div className="sns-list-card-content-sns-card w-full py-6 px-5 mt-3 bg-white ring-mainPink rounded-lg flex justify-center sm:flex-col items-center ring-4">
-                    <div className="w-3/12 sm:w-full">
-                        <div className="sns-list-card-content-sns-card-icon flex justify-center items-center">
-                            <Image
-                                src="/images/66x66.png"
-                                width={66}
-                                height={66}
-                                alt="icon"
-                            />
-                        </div>
-                        <div className="sns-list-card-content-sns-card-title hidden sm:flex sm: justify-center sm:mt-3">
-                            <h3>LINE</h3>
-                        </div>
-                    </div>
-                    <div className="sns-list-card-content-sns-card-description w-9/12 pl-2 sm:w-full sm:pl-0 sm:flex sm:justify-center sm:mt-2">
-                        <p className="text-xs tracking-wide noto-sans-jp">
-                            ひとり暮らし女性に嬉しいキャンペーン情報やおトクなクーポンをお届けします！（LINEのお友達登録要）
+                            常にリアルタイムの情報を！Xを確認しておけば貴女の希望の物件もいち早く見つかるはず！
                         </p>
                     </div>
                 </div>
