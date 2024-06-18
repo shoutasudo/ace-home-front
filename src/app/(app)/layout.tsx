38edbe9b8a3483f47aa5d0638acac29a2b6a29f0
genki1117 <88384341+genki1117@users.noreply.github.com>
2024-06-18 10:50:33 +0900

    tel link add

--- src/app/(app)/layout.tsx
+++ src/app/(app)/layout.tsx
@@ -2,12 +2,15 @@ import Footer from "@/ _components/Footer"
 import Header from "@/ _components/Header"
 import TopScrollBanner from "@/ _components/TopScrollBanner"
 import { Metadata } from "next"
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faPhoneVolume } from '@fortawesome/free-solid-svg-icons'
 
 export const metadata: Metadata = {
     title: 'TOPページ'
 }
 
-const AppLayout = ({ children }:{children: React.ReactNode}) => {
+const AppLayout = ({ children }: { children: React.ReactNode }) => {
+    const phone = '09014497070'
     return (
         <>
             <div className="w-full fixed top-0 z-50">
@@ -22,6 +25,16 @@ const AppLayout = ({ children }:{children: React.ReactNode}) => {
                     <Footer />
                 </div>
             </footer>
+            <div
+                className="fixed z-50 bottom-10 right-3 pt-5 pb-4 px-5 border-2 bg-mainPink rounded-full cursor-pointer text-white border-white"
+            >
+                <a href={`tel:${phone}`}>
+                    <FontAwesomeIcon
+                        icon={faPhoneVolume}
+                        className="w-full cursor-pointer sns-icon"
+                    />
+                </a>
+            </div>
         </>
 
     )
